68394424b758687ec2c68923704e229a6345947f
shahbazkhaliddev786 <shahbazkhalid818@gmail.com>
2025-01-11 10:48:24 +0500

    custom and ai cards responsivesness issue solved

--- components/ai-recommendation/ai.tsx
+++ components/ai-recommendation/ai.tsx
@@ -180,7 +180,7 @@ export default function CustomAIRecommendations() {
       </section>
 
       {show && (
-        <section className="w-ful flex justify-center">
+        <section className="w-ful flex flex-wrap justify-center">
           <div className="flex gap-4 flex-wrap p-6">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {perfumeData.map((card) => (
